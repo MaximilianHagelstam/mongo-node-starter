aa3fe3462d04e2f3a0edc2767897e89396d98f8c
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-10-28 10:00:29 +0300

    Add yarn and logger

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 import app from './app.js';
+import logger from './utils/logger.js';
 import { PORT } from './utils/config.js';
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () =>
+  logger.info(`Server running in ${process.env.NODE_ENV} on port ${PORT}`),
+);
